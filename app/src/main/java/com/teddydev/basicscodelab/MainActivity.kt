5126b1acc203e2015ea67ed2aacc821cea9dcd18
davide <davidemaria.colmi@gmail.com>
2022-04-12 21:09:36 +0200

    start

---
+++ app/src/main/java/com/teddydev/basicscodelab/MainActivity.kt
@@ -0,0 +1,110 @@
+package com.teddydev.basicscodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.teddydev.basicscodelab.ui.theme.BasicsCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsCodelabTheme {
+                MyApp()
+
+            }
+        }
+    }
+}
+
+@Composable
+private fun MyApp(){
+
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if(shouldShowOnboarding){
+        OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun Greetings(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if(expanded.value) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .clip(RoundedCornerShape(14.dp))
+    ) {
+        Row(modifier = Modifier
+            .padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { expanded.value = !expanded.value},
+
+            ) {
+                Text(text = if(expanded.value) "Show less" else "Show more")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, name = "default preview", widthDp = 320)
+@Composable
+private fun DefaultPreview() {
+    BasicsCodelabTheme {
+        MyApp()
+    }
+}
+
+@Composable
+private fun OnboardingScreen(onContinueClicked: () -> Unit){
+
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to the basics Codelab!")
+        Button(
+            onClick = onContinueClicked,
+            modifier = Modifier.padding(vertical = 24.dp)
+        ) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+private fun OnboardingPreview(){
+    BasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
